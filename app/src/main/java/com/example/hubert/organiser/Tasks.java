5d878cc1df31ac2fab27dcc0a3995c9934e36a0e
fiffeek <filipmikina@gmail.com>
2016-11-27 00:22:20 +0100

    taskup2

--- app/src/main/java/com/example/hubert/organiser/Tasks.java
+++ app/src/main/java/com/example/hubert/organiser/Tasks.java
@@ -10,32 +10,6 @@ import java.util.List;
  * Created by Hubert on 2016-11-25.
  */
 
-class MyComparator implements Comparator<Task> {
-
-    @Override
-    public int compare(Task obfirst, Task obsecond) {
-        if (obfirst.getPriority() > obsecond.getPriority()) {
-            return -1;
-        } else if (obfirst.getPriority() < obsecond.getPriority()) {
-            return 1;
-        } else if (obfirst.getDyear() < obsecond.getDyear()){
-            return 1;
-        }else if(obfirst.getDyear() > obsecond.getDyear()){
-            return -1;
-        }else if(obfirst.getDmonth() < obsecond.getDmonth()) {
-            return 1;
-        } else if(obfirst.getDmonth() > obsecond.getDmonth()){
-            return -1;
-        }else if(obfirst.getDday() < obsecond.getDday()){
-            return 1;
-        }else if(obfirst.getDday() > obsecond.getDday()) {
-            return -1;
-        }
-
-        return 0;
-    }
-}
-
 public class Tasks {
 
     List<Task> TaskList = new ArrayList<Task>();
@@ -46,17 +20,7 @@ public class Tasks {
     }
 
 
-   public void Task(String[] args) {
-
-        /*TaskList.add(new Task("FC Barcelona", "ops", 1, 1, 1, 1));
-        TaskList.add(new Task("Arsenal FC", "w?", 1, 2, 3, 4));
-        TaskList.add(new Task("Chelsea", "sss", 1, 2, 3, 2));*/
-
-        Collections.sort(TaskList, new MyComparator());
-
-
-        /*for (Task obie : TaskList) {
-            System.out.println(obie.Title + ": " + obie.Priority + " PRIORYTET");
-        }*/
-    }
+   public static void Task(List<Task> TaskList) {
+        Collections.sort(TaskList, new MyComp());
+   }
 }
