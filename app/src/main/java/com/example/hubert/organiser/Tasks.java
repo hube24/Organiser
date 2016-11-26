53a6cf8d1081b9d90499fb48b2f29b4ba31204d5
fiffeek <filipmikina@gmail.com>
2016-11-26 23:37:06 +0100

    kolejka - nie dziala jeszcze

--- app/src/main/java/com/example/hubert/organiser/Tasks.java
+++ app/src/main/java/com/example/hubert/organiser/Tasks.java
@@ -1,22 +1,42 @@
 package com.example.hubert.organiser;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
 
 /**
  * Created by Hubert on 2016-11-25.
  */
+/*
+class MyComparator implements Comparator<Task> {
 
-public class Tasks {
-
-    public static List <Task> TasksList;
-
-    //TODOSave to xml
-
-
-    //TODOLoad from xml
-
-
-    //TODOSort by priority
-
+    @Override
+    public int compare(Task obfirst, Task obsecond) {
+        if (obfirst.getPriority() > obsecond.getPriority()) {
+            return -1;
+        } else if (obfirst.getPriority() < obsecond.getPriority()) {
+            return 1;
+        }
+        return 0;
+    }
 }
+
+
+    public static void Task(String[] args) {
+        List<Task> TaskList = new ArrayList<Task>();
+        TaskList.add(new Task("FC Barcelona", "ops",1,1,1,1));
+        TaskList.add(new Task("Arsenal FC", "w?",1,2,3,4));
+        TaskList.add(new Task("Chelsea", "sss",1,2,3,2));
+
+        Collections.sort(TaskList, new MyComparator());
+
+        for (Task obie : TaskList) {
+            System.out.println(obie.Title + ": " + obie.Priority + " PRIORYTET");
+        }
+
+    }*/
+
+
+
