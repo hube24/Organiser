54361ecef50aa1e818e9be1faba8858fe5a4b528
fiffeek <filipmikina@gmail.com>
2016-11-26 23:49:19 +0100

    kolejka

--- app/src/main/java/com/example/hubert/organiser/Tasks.java
+++ app/src/main/java/com/example/hubert/organiser/Tasks.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * Created by Hubert on 2016-11-25.
  */
-/*
+
 class MyComparator implements Comparator<Task> {
 
     @Override
@@ -18,17 +18,31 @@ class MyComparator implements Comparator<Task> {
             return -1;
         } else if (obfirst.getPriority() < obsecond.getPriority()) {
             return 1;
+        } else if (obfirst.getDyear() < obsecond.getDyear()){
+            return 1;
+        }else if(obfirst.getDyear() > obsecond.getDyear()){
+            return -1;
+        }else if(obfirst.getDmonth() < obsecond.getDmonth()) {
+            return 1;
+        } else if(obfirst.getDmonth() > obsecond.getDmonth()){
+            return -1;
+        }else if(obfirst.getDday() < obsecond.getDday()){
+            return 1;
+        }else if(obfirst.getDday() > obsecond.getDday()) {
+            return -1;
         }
+
         return 0;
     }
 }
 
+public class Tasks {
 
     public static void Task(String[] args) {
         List<Task> TaskList = new ArrayList<Task>();
-        TaskList.add(new Task("FC Barcelona", "ops",1,1,1,1));
-        TaskList.add(new Task("Arsenal FC", "w?",1,2,3,4));
-        TaskList.add(new Task("Chelsea", "sss",1,2,3,2));
+        TaskList.add(new Task("FC Barcelona", "ops", 1, 1, 1, 1));
+        TaskList.add(new Task("Arsenal FC", "w?", 1, 2, 3, 4));
+        TaskList.add(new Task("Chelsea", "sss", 1, 2, 3, 2));
 
         Collections.sort(TaskList, new MyComparator());
 
@@ -36,4 +50,6 @@ class MyComparator implements Comparator<Task> {
             System.out.println(obie.Title + ": " + obie.Priority + " PRIORYTET");
         }
 
-    }*/
+    }
+
+}
